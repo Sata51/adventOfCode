c99535662213637ff097f51d430eaf39feedc9ef
vmathieu <virgile.mathieu@vitibot.fr>
2022-12-06 14:03:21 +0100

    day 6

--- aoc-2022/src/resolver/day/r06.rs
+++ aoc-2022/src/resolver/day/r06.rs
@@ -3,6 +3,35 @@ use crate::resolver::challenge::ChallengeResolver;
 pub struct Solver;
 
 impl ChallengeResolver for Solver {
-    fn solve1(&self, input: String) {}
-    fn solve2(&self, input: String) {}
+    fn solve1(&self, input: String) {
+        for line in input.lines() {
+            let rslt = start_of_any(line, 4);
+            println!("{}", rslt);
+        }
+    }
+    fn solve2(&self, input: String) {
+        for line in input.lines() {
+            let rslt = start_of_any(line, 14);
+            println!("{}", rslt);
+        }
+    }
+}
+
+fn start_of_any(line: &str, window_size: usize) -> i32 {
+    // Should detect the start of a packet
+    for window in line.chars().collect::<Vec<_>>().windows(window_size) {
+        // Is there any duplicate
+        if window
+            .iter()
+            .collect::<std::collections::HashSet<_>>()
+            .len()
+            == window_size
+        {
+            // println!("Last: {}", last);
+            // Get the first index of the last char in the window
+            return (line.find(&window.iter().collect::<String>()).unwrap() + window_size) as i32;
+        }
+    }
+
+    0
 }
